89b495c34af542b52f9ae8cde666655dcefb01d4
Nursultan <abdyldaev93@gmail.com>
2019-01-26 14:37:39 +0600

    add Book component

---
+++ src/Book.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types'
+
+const propTypes = {
+    book: PropTypes.object.isRequired
+};
+
+const Book = (props) => (
+    <div className="card text-center my-3">
+        <div className="card-body">
+            <div className="row">
+                <div className="col">
+                    <img src={props.book.imageLinks.smallThumbnail} height="100"/>
+                </div>
+                <div className="col-8 text-left">
+                    <h6 className="card-title">Author: <span className="font-weight-light">{props.book.authors}</span></h6>
+                    <h6 className="card-title">Title: <span className="font-weight-light">{props.book.title}</span></h6>
+                    <h6 className="card-title">Publisher: <span className="font-weight-light">{props.book.publisher}</span></h6>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+Book.propTypes = propTypes;
+
+export default Book;
